ba5539a06abd7463892404cc1e3163af3cd0ca27
obarlik <onurbarlik@hotmail.com>
2019-06-18 16:25:29 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SmtpRelay.Services;
+
+namespace SmtpRelay
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            await CreateHostBuilder(args).Build().RunAsync();
+        }
+
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseWindowsService()
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    // Configure the app here.
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<SmtpRelayService>();
+                })
+                // Only required if the service responds to requests.
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ Services/SmtpRelayService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace SmtpRelay.Services
+{
+    internal class SmtpRelayService : BackgroundService
+    {
+        public SmtpRelayService(ILoggerFactory loggerFactory)
+        {
+            Logger = loggerFactory.CreateLogger<SmtpRelayService>();
+        }
+
+        
+        public ILogger Logger { get; }
+
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            Logger.LogInformation("SmtpRelayService is starting.");
+            
+            stoppingToken.Register(() => Logger.LogInformation("SmtpRelayService is stopping."));
+            
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                Logger.LogInformation("SmtpRelayService is doing background work.");
+                
+                await Task.Delay(TimeSpan.FromSeconds(5), stoppingToken);
+            }
+            
+            Logger.LogInformation("SmtpRelayService has stopped.");
+        }
+    }
+}
